76e36c72153679fabe1692c4aa7f4d303b34d060
gohfangyao <55262977+gohfangyao@users.noreply.github.com>
2020-07-06 15:56:09 +0800

    Create Dog.java

---
+++ Dog.java
@@ -0,0 +1,9 @@
+public class Dog extends Animal //inherit from the class Animal.
+{private String d;
+public Dog (String name) {
+	d=name;
+}
+public void introduceYourself() {
+System.out.println("Woof, I am a dog. My name is"+ d);
+}
+}
