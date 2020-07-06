5ed073b7250babd68f64431a9b8b9c4f35263bc3
gohfangyao <55262977+gohfangyao@users.noreply.github.com>
2020-07-06 15:55:44 +0800

    Create Cat.java

---
+++ Cat.java
@@ -0,0 +1,10 @@
+public class Cat extends Animal //inherit from the class Animal.
+{
+	private String c;
+	public Cat (String name) {
+		c=name;
+	}
+public void introduceYourself() {
+	System.out.println("Meow, I am a cat. My name is"+ c);
+}
+}
