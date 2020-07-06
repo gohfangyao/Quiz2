bb4a047c713cac1f5293e3765cc5553ba0f82ee9
gohfangyao <55262977+gohfangyao@users.noreply.github.com>
2020-07-06 16:28:23 +0800

    Update Dog.java

--- Dog.java
+++ Dog.java
@@ -4,6 +4,6 @@ public Dog (String name) {
 	d=name;
 }
 public void introduceYourself() {
-System.out.println("Woof. I am a dog. My name is"+ d);
+System.out.println("Woof. I am a dog. My name is"+ d +".");
 }
 }
